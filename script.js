76af6682f31614c9d1ae8e528db566ed3c6290a5
Kimberly Duffy <kymberli@honeycrm.com>
2020-01-30 10:26:28 -0600

    Updated some styling as well as working on delete functionality.

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function addTodo() {
     var newTodo = todoInput.value;
 
     var todoItem = {
+        id: generateId(),
         name: newTodo,
-        checked: false,
-        id: Date.now
+        checked: false
     }
 
     todoItems.push(todoItem);
@@ -16,17 +16,43 @@ function addTodo() {
     clearInputs();
 }
 
+function generateId() {
+    return Math.random().toString(36).substring(2, 15);
+}
 
-function completeTodo() {
+function toggleTodo() {
+    var id = this.parentElement.id;
+    var todo = getTodoById(id);
 
+    todo.checked = !todo.checked;
+
+    if(todo.checked) {
+        document.getElementById(todo.id).classList.add('done');
+    } else {
+        document.getElementById(todo.id).classList.remove('done');
+    }
 }
 
 function deleteTodo() {
+    var id = this.parentElement.id;
+    var todoAddress = todoItems.indexOf(getTodoById(id));
 
+    todoItems.splice(todo, 1);
+    alert('deleted!');
 }
 
 function editTodo() {
-    
+    var id = this.parentElement.id;
+    var todo = getTodoById(id);
+    alert('edited!');
+}
+
+function getTodoById(id) {
+    for(var i = 0; i < todoItems.length; i++) {
+        if(id === todoItems[i].id) {
+            return todoItems[i];
+        }
+    }
 }
 
 function clearInputs() {
@@ -40,13 +66,19 @@ function renderTodos() {
 
     for(var i = 0; i < todoItems.length; i++) {
         var todoItem = todoItems[i];
-        html += `<li>${ todoItem.name }</li>`;
+        html += `<li class="todo" id="${ todoItem.id }"><input class="checkbox" type="checkbox"> ${ todoItem.name } <button class="delete btn"><i class="fa fa-trash"></i></button></li>`;
     }
 
     todoListEl.innerHTML = html;
+
+    // add change event
+    var checkboxes = document.getElementsByClassName('checkbox');
+    for(var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', toggleTodo);
+    }
+
+    var deleteBoxes = document.getElementsByClassName('delete');
+    for(var i = 0; i < deleteBoxes.length; i++) {
+        deleteBoxes[i].addEventListener('click', deleteTodo);
+    }
 }
-
-
-// check off todo
-// delete todo
-// edit todo
